b9ed368c56210c83fb2af0e9f2fa40904caca5a3
grainier <grainier.perera@gmail.com>
2025-07-08 21:14:41 +0530

    Add lab implementation

--- lab1/part1/do-lab.sh
+++ lab1/part1/do-lab.sh
@@ -3,3 +3,14 @@
 # INCLUDE ALL COMMANDS NEEDED TO PERFORM THE LAB
 # This file will get called from capture_submission.sh
 
+docker exec clab-lab1-part1-switch ip link add name br0 type bridge
+docker exec clab-lab1-part1-switch ip link set br0 up
+docker exec clab-lab1-part1-switch ip link set eth1 master br0
+docker exec clab-lab1-part1-switch ip link set eth2 master br0
+docker exec clab-lab1-part1-switch ip link set eth3 master br0
+docker exec clab-lab1-part1-switch ip link set eth4 master br0
+
+docker exec clab-lab1-part1-switch ip link set eth1 up
+docker exec clab-lab1-part1-switch ip link set eth2 up
+docker exec clab-lab1-part1-switch ip link set eth3 up
+docker exec clab-lab1-part1-switch ip link set eth4 up
